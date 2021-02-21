2db4a36ce9c44398312a54a7b21513c0d6fc0a09
Jason Rudolph <jason@jasonrudolph.com>
2021-02-21 09:46:46 -0500

    Update "dictionary" to use muan/emojilib v3.0.0
    /xref https://github.com/muan/emojilib/releases/tag/v3.0.0

---
+++ emoji-lookup.lbaction/Contents/Scripts/emoji-dictionary.js
@@ -0,0 +1,28 @@
+// Translates the data from muan/emojilib for use as a "dictionary" of emojis.
+//
+// Returns an Object describing all the supported emojis. Each key is a String
+//   representing the snake_case name of the emoji. Each value is an Object
+//   containing the metadata for that emoji (i.e., the emoji character and the
+//   emoji's keywords). Example:
+//
+//   {
+//     "grinning_face_with_sweat": {
+//       "char": "😅",
+//       "keywords": ["face", "hot", "happy", "laugh", "sweat", "smile", "relief"]
+//     },
+//     "man_dancing": {
+//       "char": "🕺",
+//       "keywords": ["male", "boy", "fun", "dancer"]
+//     }
+//   }
+function getDictionary(nameAndKeywordsByChar) {
+  const dictionary = {}
+  for (const char in nameAndKeywordsByChar) {
+    const name = nameAndKeywordsByChar[char][0]
+    const keywords = nameAndKeywordsByChar[char].slice(1)
+    dictionary[name] = { 'char': char, 'keywords': keywords }
+  }
+  return dictionary
+}
+
+if (typeof module !== 'undefined') { module.exports = { getDictionary } }
--- test/emoji-character.test.js
+++ test/emoji-character.test.js
@@ -2,20 +2,22 @@ const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 const {EmojiCharacter} = require('../emoji-lookup.lbaction/Contents/Scripts/emoji-character.js')
-const dictionaryPath = path.join(__dirname, '../emoji-lookup.lbaction/Contents/vendor/emojilib/emojis.json')
+const {getDictionary} = require('../emoji-lookup.lbaction/Contents/Scripts/emoji-dictionary.js')
+const emojilibDataPath = path.join(__dirname, '../emoji-lookup.lbaction/Contents/vendor/emojilib/emoji-en-US.json')
 
 describe('EmojiCharacter', () => {
   let dictionary
 
   beforeEach(() => {
-    dictionary = JSON.parse(fs.readFileSync(dictionaryPath))
+    const nameAndKeywordsByChar = JSON.parse(fs.readFileSync(emojilibDataPath))
+    dictionary = getDictionary(nameAndKeywordsByChar)
   })
 
   describe('#launchbarIcon', () => {
     it('returns path for single-codepoint emoji', () => {
       const character = new EmojiCharacter({
-        name: 'boom',
-        metadata: dictionary['boom'],
+        name: 'collision',
+        metadata: dictionary['collision'],
         resourcesPath: '/path/to/resources/'
       })
       assert.equal(character.launchbarIcon(), '/path/to/resources/unicode/1f4a5.png')
@@ -23,8 +25,8 @@ describe('EmojiCharacter', () => {
 
     it('returns path for multi-codepoint emoji ', () => {
       const character = new EmojiCharacter({
-        name: 'female_detective',
-        metadata: dictionary['female_detective'],
+        name: 'woman_detective',
+        metadata: dictionary['woman_detective'],
         resourcesPath: '/path/to/resources/'
       })
       assert.equal(character.launchbarIcon(), '/path/to/resources/unicode/1f575-2640.png')
