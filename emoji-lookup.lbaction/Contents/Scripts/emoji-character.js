5819c51db864c6da024b9b33b45ba67bd6a8e8fd
Jason Rudolph <jason@jasonrudolph.com>
2025-04-21 11:00:07 -0700

    🔥 Remove obsolete code re: to unicodeCodePoints
    Following the changes in #21, we're no longer using this code.

--- emoji-lookup.lbaction/Contents/Scripts/emoji-character.js
+++ emoji-lookup.lbaction/Contents/Scripts/emoji-character.js
@@ -21,45 +21,6 @@ class EmojiCharacter {
     return this.data["keywords"]
   }
 
-  // Returns an Array of Strings.
-  get unicodeCodePoints() {
-    if (this.character == null) {
-      return []
-    } else {
-      // Most emojis have only a single codepoint, but some have more.
-      //
-      // Country flags use two codepoints, determined by the ISO 3166-1
-      // two-character country code. For example, the US flag emoji is:
-      // U+1F1FA (for "U") followed by U+1F1F8 (for "S").
-      //
-      // Many of the people emojis are made up of several codepoints. For
-      // example, the female sleuth with no skin tone is made up of the
-      // following five codepoints:
-      // - U+1F575 Sleuth
-      // - U+FE0F  Variation selector 16 (http://emojipedia.org/variation-selector-16/)
-      // - U+200D  Zero-width joiner
-      // - U+2640  Female sign ♀
-      // - U+FE0F  Variation selector 16 (http://emojipedia.org/variation-selector-16/)
-      //
-      // For our purposes, we don't care about the variation selector or the
-      // zero-width joiner, so we'll discard them (if present) and get the
-      // remaining codepoints.
-      const VARIATION_SELECTOR_16 = "\ufe0f"
-      const ZERO_WIDTH_JOINER = "\u200d"
-
-      const parts = [...this.character]
-      const relevantParts = parts.filter(
-        (part) => part !== VARIATION_SELECTOR_16 && part !== ZERO_WIDTH_JOINER
-      )
-
-      const codePointHexValues = relevantParts.map((part) =>
-        part.codePointAt(0).toString(16)
-      )
-
-      return codePointHexValues
-    }
-  }
-
   toLaunchbarItem() {
     return {
       title: this.launchbarItemTitle(),
@@ -75,11 +36,7 @@ class EmojiCharacter {
   }
 
   launchbarItemLabel() {
-    if (this.isUnicode()) {
-      return ""
-    } else {
-      return "custom"
-    }
+    return ""
   }
 
   launchbarItemIcon() {
@@ -92,10 +49,6 @@ class EmojiCharacter {
     } else {
       return this.humanizedName
     }
-  }
-
-  isUnicode() {
-    return this.unicodeCodePoints.length > 0
   }
 }
 
